562d518e630419f4892f3f2f8920e0f7d547f30a
Ash <Ashbending@gmail.com>
2021-09-13 22:21:36 +0100

    Issues with maven and spring boot meant the project had to be recreated.
    Functionally the same, with the full test suite included

---
+++ src/main/java/com/amash/CardGenerationServlet/CardGenerationServletApplication.java
@@ -0,0 +1,15 @@
+package com.amash.CardGenerationServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGenerationServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardGenerationServletApplication.class, args);
+	}
+
+	public static void placeholder(){
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/com/amash/CardGenerationServlet/CardGenerationServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amash.CardGenerationServlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGenerationServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
