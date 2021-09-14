f28cba513e22c05c3c2b94b3387678ee5c088848
Ash <Ashbending@gmail.com>
2021-09-14 19:21:04 +0100

    Jenkins part 2.
    Resolving issues with test builds is kind of fun

--- src/test/java/com/amash/CardGenerationServlet/CardGenerationServletApplicationTests.java
+++ src/test/java/com/amash/CardGenerationServlet/CardGenerationServletApplicationTests.java
@@ -1,9 +1,8 @@
 package com.amash.CardGenerationServlet;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CardGenerationServletApplicationTests {
 
 	@Test
